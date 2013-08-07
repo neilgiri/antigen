2b6452f85dc200cbf39464276637edcf444342a8
Trevor Bedford <trvrb@trevorbedford.com>
2013-08-07 12:38:55 -0700

    Update parameters for simple analysis.

--- Parameters.java
+++ Parameters.java
@@ -10,15 +10,15 @@ public class Parameters {
 	public static int day = 0;
 	public static final int burnin = 0; // 0
 	public static final int endDay = 3*365; // 20*365 = 7300
-	public static final int printStep = 7;								// print to out.timeseries every day
-	public static final double tipSamplingRate = 0.0002;				// in samples per deme per day
+	public static final int printStep = 10;								// print to out.timeseries every day
+	public static final double tipSamplingRate = 1;						// in samples per deme per day
 	public static final int tipSamplesPerDeme = 2000;
-	public static final boolean tipSamplingProportional = true;			// whether to sample proportional to prevalance
-	public static final double treeProportion = 0.1;					// proportion of tips to use in tree reconstruction
+	public static final boolean tipSamplingProportional = false;		// whether to sample proportional to prevalance
+	public static final double treeProportion = 1;						// proportion of tips to use in tree reconstruction
 	public static final int	diversitySamplingCount = 1000;				// how many samples to draw to calculate diversity
-	public static final int	netauSamplingCount = 10000;					// how many samples to draw to calculate Ne*tau	
-	public static final double	netauWindow = 7;						// window in days to calculate Ne*tau	
-	public static final int	serialIntervalSamplingCount = 10000;		// how many samples to draw to calculate serial interval	
+	public static final int	netauSamplingCount = 1000000;				// how many samples to draw to calculate Ne*tau	
+	public static final double	netauWindow = 10;						// window in days to calculate Ne*tau	
+	public static final int	serialIntervalSamplingCount = 1000000;		// how many samples to draw to calculate serial interval	
 	public static final boolean repeatSim = false;						// repeat simulation until endDay is reached?
 	public static final boolean memoryProfiling = false;				// requires -javaagent:classmexer.jar to run
 
@@ -27,7 +27,7 @@ public class Parameters {
 	// metapopulation parameters
 	public static final int demeCount = 1;
 	public static final String[] demeNames = {"world"};
-	public static final int[] initialNs = {5000000};	
+	public static final int[] initialNs = {1000000};	
 	
 	// host parameters
 	public static final double birthRate = 0.000091;				// in births per individual per day, 1/30 years = 0.000091
@@ -35,11 +35,11 @@ public class Parameters {
 	public static final boolean swapDemography = true;				// whether to keep overall population size constant
 		
 	// epidemiological parameters
-	public static final int initialI = 10;							// in individuals
+	public static final int initialI = 1;							// in individuals
 	public static final double initialPrR = 0.0; 					// as proportion of population
 	public static final double beta = 0.3; 							// in contacts per individual per day
 	public static final double nu = 0.2; 							// in recoveries per individual per day
-	public static final double immunityLoss = 0.0027;				// in R->S per individual per day
+	public static final double immunityLoss = 0.00274;				// in R->S per individual per day
 	public static final double betweenDemePro = 0.0005;				// relative to within-deme beta		
 	
 	// seasonal betas
